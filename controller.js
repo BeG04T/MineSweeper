ffb69c9a23354bbf9e4ded465255306f8a2ce6e7
BeG04T <ericcenuniversity@gmail.com>
2022-05-28 17:49:09 -0400

    added Restart feature.

--- controller.js
+++ controller.js
@@ -7,10 +7,13 @@ $(document).ready(function(){
 
     $("#start").click(_ => {create_game()});
 
+    $("#restart").click(_ => {create_game()});
+
     const create_game = _ => {
         game = new Game(set_minecount)
 
         $("#start").hide()
+        $("#restart").show()
         $("#GameBoard").html("<div id=\"MineField\">" +
     "</div>")
 
@@ -36,6 +39,7 @@ $(document).ready(function(){
       const nearby_Mines = game.clickTile(parseInt(axis[0]), parseInt(axis[1]))
       if (nearby_Mines < 0) {
         $(this).css("color", "red")
+        $("#restart").hide()
         $("#start").show()
       }
       $(this).html(nearby_Mines)
@@ -48,6 +52,7 @@ $(document).ready(function(){
         }}
       }
       if (game.tiles == 0){
+        $("#restart").hide()
         $("#start").show()
       }
     });
